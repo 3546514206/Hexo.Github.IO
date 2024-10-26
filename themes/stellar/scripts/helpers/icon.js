31c604200dba6600f6364b9e785380b04dc2166f
杨海波 <3546514206@qq.com>
2024-10-27 01:52:52 +0800

    删除其余不用主题

--- themes/stellar/scripts/helpers/icon.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-hexo.extend.helper.register('icon', function (key, args) {
-    return hexo.utils.icon(key, args)
-})
--- themes/stellar/scripts/helpers/scrollreveal.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-hexo.extend.helper.register('scrollreveal', function (args) {
-    if (hexo.theme.config.plugins.scrollreveal?.enable) {
-        return `${args ? args : ''}slide-up`
-    } else {
-        return ''
-    }
-})
